6b1b2906d85089259cd31491c64af8d8faf85198
Reza12MQ <reza_muqu@rocketmail.com>
2023-05-22 18:31:16 +0700

    laratrust setup

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laratrust\Contracts\LaratrustUser;
+use Laratrust\Traits\HasRolesAndPermissions;
 
-class User extends Authenticatable
+class User extends Authenticatable implements LaratrustUser
 {
+    use HasRolesAndPermissions;
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
