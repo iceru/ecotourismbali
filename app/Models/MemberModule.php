eaab97f44a3a8d8e1e7c4553b9e6453db035ec60
Reza12MQ <reza_muqu@rocketmail.com>
2023-05-23 19:27:02 +0700

    module migrations & models

---
+++ app/Models/MemberModule.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class MemberModule extends Model
+{
+    protected $table = 'member_modules';
+
+    public function member(): BelongsTo
+    {
+        return $this->belongsTo(Member::class, 'member_id');
+    }
+
+    public function module(): BelongsTo
+    {
+        return $this->belongsTo(Module::class, 'module_id');
+    }
+}
---
+++ app/Models/Module.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Module extends Model
+{
+    protected $table = 'modules';
+
+    public function member_module(): HasMany
+    {
+        return $this->hasMany(MemberModule::class, 'module_id');
+    }
+}
