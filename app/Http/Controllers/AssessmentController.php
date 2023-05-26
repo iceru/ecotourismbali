14df160cf7d9ea447223b52f230c48ab4b0c3076
Reza12MQ <reza_muqu@rocketmail.com>
2023-05-26 15:01:40 +0700

    assessment index & store

---
+++ app/Http/Controllers/AssessmentController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Assessment;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+
+class AssessmentController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Admin/Assessment/CreateAssessment', [
+            'assessment' => Assessment::all(),
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $assessment = new Assessment;
+
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'image' => 'required|image',
+        ]);
+
+        if ($request->hasFile('key')) {
+            $extension = $request->file('image')->extension();
+            $filename = $request->name.'_'.time().'.'.$extension;
+            $path = $request->file('image')->storeAs('public/assessments', $filename);
+        }
+
+        $assessment->title = $request->name;
+        $assessment->description = $request->description;
+        $assessment->image = $request->image;
+        $assessment->save();
+
+        return Redirect::route('assessment.index');
+    }
+}
