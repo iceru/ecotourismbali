b7e588e807dbc735c39f95e00a8dc6ea96f17165
Muhamad Hafiz <m.hafiz1825@gmail.com>
2023-07-04 21:59:56 +0700

    update build

---
+++ public/build/assets/InputLabel-caac67f0.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-e318cb8a.js";function l({value:t,className:e="",children:r,...a}){return n("label",{...a,className:"block font-medium text-gray-700 "+e,children:t||r})}export{l as I};
--- public/build/assets/Welcome-5f46ce08.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as o,F as r}from"./app-6dc2ec92.js";function a(){return e.useEffect(()=>{window.location.href="/login"},[]),o(r,{})}export{a as default};
