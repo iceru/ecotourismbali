219d6623b93cbb116d642b2da9a220ec3b32b9db
Muhamad Hafiz <m.hafiz1825@gmail.com>
2023-07-16 11:11:40 +0700

    add setup analytics

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
+use App\Models\Member;
+use App\Models\Module;
 use Illuminate\Http\Request;
+use Spatie\Analytics\Period;
+use Spatie\Analytics\Facades\Analytics;
 
 class AdminDashboardController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Admin/AdminDashboard');
+        $visitors = Analytics::fetchVisitorsAndPageViews(Period::days(7));
+        return Inertia::render('Admin/AdminDashboard', [
+            'members' => Member::all(),
+            'modules' => Module::all(),
+            'visitors' => $visitors,
+        ]);
     }
 }
--- resources/js/Pages/Admin/AdminDashboard.jsx
+++ resources/js/Pages/Admin/AdminDashboard.jsx
@@ -4,22 +4,26 @@ import { useTranslation } from 'react-i18next';
 import TitleSection from './Components/TitleSection';
 import PrimaryButton from '@/Components/PrimaryButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Bar } from 'react-chartjs-2';
 import { faFileDownload } from '@fortawesome/free-solid-svg-icons';
+import { Chart as ChartJS, registerables } from 'chart.js';
+import { Chart } from 'react-chartjs-2';
 
-function MemberDashboard() {
+function MemberDashboard({ members, modules, visitors }) {
   const { t } = useTranslation();
-
+  console.log(visitors);
+  ChartJS.register(...registerables);
   return (
     <AdminLayout>
       <div className="grid lg:grid-cols-2 gap-6">
         <AdminSection className="grid lg:grid-cols-2 gap-4">
           <div className="bg-lightSecondary p-4 rounded-md">
             <div className="mb-2">{t('total_members')}</div>
-            <div className="font-bold text-3xl">120</div>
+            <div className="font-bold text-3xl">{members.length}</div>
           </div>
           <div className="bg-lightPrimary p-4 rounded-md">
             <div className="mb-2">{t('total_module')}</div>
-            <div className="font-bold text-3xl">5</div>
+            <div className="font-bold text-3xl">{modules.length}</div>
           </div>
         </AdminSection>
         <AdminSection>
@@ -32,6 +36,29 @@ function MemberDashboard() {
             {t('download_guide_book')}
             <FontAwesomeIcon icon={faFileDownload} />
           </PrimaryButton>
+        </AdminSection>
+        <AdminSection>
+          <Bar
+            datasetIdKey="id"
+            data={{
+              datasets: [
+                {
+                  barPercentage: 0.5,
+                  barThickness: 6,
+                  maxBarThickness: 8,
+                  minBarLength: 2,
+                  data: [10, 20, 30, 40, 50, 60, 70],
+                },
+              ],
+              options: {
+                scales: {
+                  y: {
+                    beginAtZero: true,
+                  },
+                },
+              },
+            }}
+          />
         </AdminSection>
       </div>
     </AdminLayout>
