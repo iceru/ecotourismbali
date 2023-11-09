f36e23a58dffe805098e5fa42c9d2fc72c38492c
Muhamad Hafiz <m.hafiz1825@gmail.com>
2023-11-09 22:16:39 +0700

    update build

---
+++ public/build/assets/TitleSection-ae32a14d.js
@@ -0,0 +1,1 @@
+import{a as t}from"./app-577e16bd.js";import{u as n}from"./useTranslation-bb9bfc0a.js";function s({title:o,className:e}){const{t:i}=n();return t("div",{children:t("div",{className:`font-bold text-xl ${e}`,children:i(o)})})}export{s as default};
