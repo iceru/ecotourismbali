893857db17c5c48f27911ebdad0a6a4dc3a935d4
Muhamad Hafiz <m.hafiz1825@gmail.com>
2023-11-27 21:50:17 +0700

    update build

---
+++ public/build/assets/AdminSection-b89f24b1.js
@@ -0,0 +1,1 @@
+import{a as d}from"./app-4965786e.js";function n({children:o,className:a=""}){return d("div",{className:`bg-white drop-shadow-admin p-6 rounded-2xl ${a}`,children:o})}export{n as A};
