d0409a08a41e1ac22b1623d1e363b146c62dc730
Muhamad Hafiz <m.hafiz1825@gmail.com>
2024-01-25 00:53:10 +0700

    update build

--- public/build/assets/InputLabel-4903fbc1.js
+++
@@ -1,1 +0,0 @@
-import{a as n}from"./app-0c39fb5f.js";function l({value:t,className:a="",children:e,...r}){return n("label",{...r,className:"block font-medium text-gray-700 "+a,children:t||e})}export{l as I};
--- public/build/assets/TitleSection-7303c6ab.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./app-0c39fb5f.js";import{u as n}from"./useTranslation-e654b818.js";function s({title:o,className:e}){const{t:i}=n();return t("div",{children:t("div",{className:`font-bold text-xl ${e}`,children:i(o)})})}export{s as default};
