883cafebe00af096f385ac5acebd017232046e6b
Muhamad Hafiz <m.hafiz1825@gmail.com>
2024-07-06 06:12:32 +0700

    update navbar

---
+++ public/build/assets/InputLabel-81727dd6.js
@@ -0,0 +1,1 @@
+import{a as n}from"./app-c38276ce.js";function l({value:t,className:a="",children:e,...r}){return n("label",{...r,className:"block font-medium text-gray-700 "+a,children:t||e})}export{l as I};
--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link, usePage } from '@inertiajs/react';
 import { useTranslation } from 'react-i18next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -14,14 +14,27 @@ import Logo from '../../images/logo.png';
 function Navbar() {
   const { i18n } = useTranslation();
   const [sidebar, setSidebar] = useState(false);
-  const [about, setAbout] = useState(false);
   const [programs, setPrograms] = useState(false);
-  const [tribe, setTribe] = useState(false);
-  const [blog, setBlog] = useState(false);
   const { auth } = usePage().props;
   const { t } = useTranslation();
   const [openProducts, setOpenProducts] = useState(false);
   const { url } = usePage();
+
+  const sidebarRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = event => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        setSidebar(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
     <header className="py-2 bg-white">
@@ -45,7 +58,7 @@ function Navbar() {
                 className="flex gap-1 items-center"
                 onClick={() => setOpenProducts(!openProducts)}
               >
-                Products & Events
+                What We Offer
                 <FontAwesomeIcon icon={faChevronDown} className="text-[10px]" />
               </button>
             </li>
@@ -58,18 +71,21 @@ function Navbar() {
             >
               <div className="container flex justify-center gap-20 py-6">
                 <div>
-                  <a
-                    className="text-primary text-2xl mb-3 block font-serif"
-                    href="https://ecotourimbali.com/programs"
-                  >
-                    Products
-                  </a>
+                  <div className="text-primary text-2xl mb-3 block font-serif">
+                    What We Offer
+                  </div>
+                  <li>
+                    <a href="https://ecotourimbali.com/eco-climate-badge">
+                      Eco Climate Badge
+                    </a>
+                  </li>
+                </div>
+                <div className="w-[1px] bg-gray-300"></div>
+                <div>
+                  <div className="text-primary text-2xl mb-3 block font-serif">
+                    Programs
+                  </div>
                   <ul className="grid gap-2">
-                    <li>
-                      <a href="https://ecotourimbali.com/eco-climate-badge">
-                        Eco Climate Badge
-                      </a>
-                    </li>
                     <li>
                       <a href="https://ecotourimbali.com/regenerative-bali">
                         Regenerative Bali
@@ -119,19 +135,11 @@ function Navbar() {
               </Link>
             </li>
 
-            <li className="relative group">
-              <span>Insights</span>
-              <ul className="hidden absolute top-2 pt-8 group-hover:block hover:block">
-                <li className="py-3 px-5 whitespace-nowrap hover:bg-gray-100 hover:text-primary bg-white">
-                  <a href="https://ecotourismbali.com/blog/">{t('blog')}</a>
-                </li>
-                <li className="py-3 px-5 whitespace-nowrap hover:bg-gray-100 hover:text-primary bg-white">
-                  <a href="https://ecotourismbali.com/news/">{t('news')}</a>
-                </li>
-              </ul>
+            <li className="">
+              <a href="https://ecotourismbali.com/insights">Insights</a>
             </li>
           </ul>
-          <div onClick={() => setSidebar(true)} className="lg:hidden">
+          <div onClick={() => setSidebar(!sidebar)} className="lg:hidden">
             <FontAwesomeIcon icon={faBars} className="text-3xl text-primary" />
           </div>
         </div>
@@ -172,206 +180,128 @@ function Navbar() {
             </div>
           </div>
         ) : (
-          <Link href={route('member.dashboard')}>
-            <li className="flex items-center gap-2 text-primary">
-              <Link
-                href="/register"
-                className={url === '/register' ? 'font-bold' : ''}
-              >
-                Register
-              </Link>
-              <span>|</span>
-              <Link
-                href="/login"
-                className={url === '/login' ? 'font-bold' : ''}
-              >
-                Sign In
-              </Link>
-            </li>
-          </Link>
+          <li className="items-center gap-2 text-primary hidden lg:flex">
+            <Link
+              href="/register"
+              className={url === '/register' ? 'font-bold' : ''}
+            >
+              Register
+            </Link>
+            <span>|</span>
+            <Link href="/login" className={url === '/login' ? 'font-bold' : ''}>
+              Sign In
+            </Link>
+          </li>
         )}
       </nav>
       <div
-        className={`bg-primary transition  text-white w-full h-full fixed z-40 top-0 left-0 p-6 pt-0 ${
+        className={`sidebar fixed overflow-auto top-0 left-0 w-[80vw] duration-500 flex flex-col justify-between h-screen transition bg-white z-40 p-6 ${
           sidebar ? 'translate-x-0' : '-translate-x-full'
         }`}
+        ref={sidebarRef}
       >
-        <div className="bg-white flex justify-between items-center p-3 mb-12 -mx-6">
-          <img src={Logo} alt="Eco Tourism Bali" className="w-[80px]" />
-          <div onClick={() => setSidebar(false)}>
-            <FontAwesomeIcon
-              icon={faTimes}
-              className="text-3xl text-primary cursor-pointer"
-            />
+        <div class="mb-6">
+          <div class="logo w-24 lg:w-32 mb-14">
+            <a href="/" class="">
+              <img class="h-16 lg:h-20" src={Logo} alt="Eco Tourism Bali" />
+            </a>
           </div>
+          <nav>
+            <ul class="flex flex-col  gap-6">
+              <li>
+                <a class="menulinks" href="/">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a class="menulinks" href="/about/">
+                  About
+                </a>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  class="flex items-center gap-1 sideservice"
+                  onClick={() => setPrograms(!programs)}
+                >
+                  What We Offer
+                  <FontAwesomeIcon
+                    icon={faChevronDown}
+                    className="text-xs ml-1"
+                  />
+                </button>
+                <div class={`pl-4 mt-4 ${programs ? '' : 'hidden'}`}>
+                  <ul class="grid gap-4 mb-4 pb-4 border-b border-gray-100">
+                    <li class="text-primary font-bold">What We Offer</li>
+                    <li>
+                      <a href="/eco-climate-badge">Eco Climate Badge</a>
+                    </li>
+                  </ul>
+                  <ul class="grid gap-4 mb-4 pb-4 border-b border-gray-100">
+                    <li>
+                      <a href="/products" class="text-primary font-bold">
+                        Products
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/regenerative-bali">Regenerative Bali</a>
+                    </li>
+                    <li>
+                      <a href="/products#training">Training & Consultancy</a>
+                    </li>
+                    <li>
+                      <a href="/products#sustainable">
+                        Sustainable Event Management
+                      </a>
+                    </li>
+                  </ul>
+                  <ul class="grid gap-4">
+                    <li>
+                      <a href="/events" class="text-primary font-bold">
+                        Events
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/events#etw">Eco Tourism Week</a>
+                    </li>
+                    <li>
+                      <a href="/events#calendar">Event Calendar</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <a
+                  class="menulinks"
+                  href="https://member.ecotourismbali.com/directory"
+                >
+                  Directory
+                </a>
+              </li>
+              <li>
+                <a class="menulinks" href="/insights/">
+                  Insights
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
 
-        <ul className=" gap-8 flex flex-col uppercase text-[15px]">
-          <li className="relative group">
-            <span
-              onClick={() => setAbout(!about)}
-              className="cursor-pointer flex items-center"
-            >
-              {t('about')}
-              <FontAwesomeIcon className="ml-2 text-xs" icon={faChevronDown} />
-            </span>
-            <ul className={`${about ? 'block' : 'hidden'} pt-3 -mb-4`}>
-              <li className="py-3 px-5 whitespace-nowrap ">
-                <a href="https://ecotourismbali.com/about/climate-ambition/">
-                  Climate Ambition
-                </a>
-              </li>
-              <li className="py-3 px-5 whitespace-nowrap ">
-                <a href="https://ecotourismbali.com/about/team/">Our Team</a>
-              </li>
-              <li className="py-3 px-5 whitespace-nowrap ">
-                <a href="https://ecotourismbali.com/about/partners/">
-                  Partners
-                </a>
-              </li>
-              <li className="py-3 px-5 whitespace-nowrap ">
-                <a href="https://ecotourismbali.com/about/pillars/">Pillars</a>
-              </li>
-              <li className="py-3 px-5 whitespace-nowrap ">
-                <a href="https://ecotourismbali.com/about/etb-road-map/">
-                  Road Map
-                </a>
-              </li>
-            </ul>
-          </li>
-
-          <li className="relative group">
-            <span
-              onClick={() => setPrograms(!programs)}
-              className="cursor-pointer flex items-center"
-            >
-              Programs
-              <FontAwesomeIcon className="ml-2 text-xs" icon={faChevronDown} />
-            </span>
-            <ul className={`${programs ? 'block' : 'hidden'} pt-3 -mb-4`}>
-              <li className="py-3 px-5 whitespace-nowrap ">
-                <a href="https://ecotourismbali.com/sustainable-verification/">
-                  Sustainable Verification
-                </a>
-              </li>
-              <li className="py-3 px-5 whitespace-nowrap ">
-                <a href="https://joinetw.ecotourismbali.com/">
-                  Eco Tourism Week
-                </a>
-              </li>
-              <li className="py-3 px-5 whitespace-nowrap ">
-                <a href="https://ecotourismbali.com/programs/regenerative-bali/">
-                  Regenerative Bali
-                </a>
-              </li>
-              <li className="py-3 px-5 whitespace-nowrap ">
-                <a href="https://ecotourismbali.com/programs/workshop/">
-                  Workshop
-                </a>
-              </li>
-              <li className="py-3 px-5 whitespace-nowrap ">
-                <a href="https://ecotourismbali.com/programs/consultation/">
-                  Consulting
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li className="relative group">
-            <span
-              onClick={() => setTribe(!tribe)}
-              className="cursor-pointer flex items-center"
-            >
-              Eco Tourism Tribe
-              <FontAwesomeIcon className="ml-2 text-xs" icon={faChevronDown} />
-            </span>
-            <ul className={`${tribe ? 'block' : 'hidden'} pt-3 -mb-4`}>
-              <li className="py-3 px-5 whitespace-nowrap  flex items-center w-full">
-                <img
-                  src="https://ecotourismbali.com/wp-content/uploads/2022/11/Icon_greenPAL_1-36x36.png"
-                  width="36"
-                  height="36"
-                  className="mr-3 brightness-0 transform invert"
-                />
-                <a href="https://ecotourismbali.com/tribe/community/">
-                  Community
-                </a>
-              </li>
-              <li className="py-3 px-5 whitespace-nowrap  flex items-center">
-                <img
-                  src="https://ecotourismbali.com/wp-content/uploads/2022/11/Icon_greenforce_1-36x36.png"
-                  className="mr-3 brightness-0 transform invert"
-                  width="36"
-                  height="36"
-                />
-                <a href="https://ecotourismbali.com/tribe/member/">Member</a>
-              </li>
-              <li className="py-3 px-5 whitespace-nowrap  flex items-center">
-                <img
-                  src="https://ecotourismbali.com/wp-content/uploads/2022/11/Icon_earth_guard_1-36x36.png"
-                  className="mr-3 brightness-0 transform invert"
-                  width="36"
-                  height="36"
-                />
-                <a href="https://ecotourismbali.com/tribe/eco-tourism-partners/">
-                  Partners
-                </a>
-              </li>
-              <li className="py-3 px-5 whitespace-nowrap  flex items-center w-full min-w-[230px]">
-                <img
-                  src="https://ecotourismbali.com/wp-content/uploads/2022/11/Icon_greenAvenger_1-36x36.png"
-                  className="mr-3 brightness-0 transform invert"
-                  width="36"
-                  height="36"
-                />
-                <a href="https://ecotourismbali.com/tribe/verified-member/">
-                  Verified Members
-                </a>
-              </li>
-              <li className="py-3 px-5 whitespace-nowrap  flex items-center">
-                <img
-                  src="https://ecotourismbali.com/wp-content/uploads/2022/11/Icon_etb_ambass_1-36x36.png"
-                  className="mr-3 brightness-0 transform invert"
-                  width="36"
-                  height="36"
-                />
-                <a href="https://ecotourismbali.com/tribe/ambassadors/">
-                  Ambassadors
-                </a>
-              </li>
-              <li className="py-3 px-5 whitespace-nowrap  flex items-center">
-                <img
-                  src="https://ecotourismbali.com/wp-content/uploads/2022/11/Icon_warrior_1-36x36.png"
-                  className="mr-3 brightness-0 transform invert"
-                  width="36"
-                  height="36"
-                />
-                <a href="https://ecotourismbali.com/tribe/warriors/">
-                  Warriors
-                </a>
-              </li>
-            </ul>
-          </li>
-          <a href="https://ecotourismbali.com/sustainable-verification/">
-            <li className="">Sustainable Verification</li>
+        <div class="gap-3 flex text-primary">
+          <a
+            href="https://member.ecotourismbali.com/register"
+            class="whitespace-nowrap"
+          >
+            Register
           </a>
-          <li className="relative group">
-            <span
-              onClick={() => setBlog(!blog)}
-              className="cursor-pointer flex items-center"
-            >
-              Blog And News
-              <FontAwesomeIcon className="ml-2 text-xs" icon={faChevronDown} />
-            </span>
-            <ul className={`${blog ? 'block' : 'hidden'} pt-3 -mb-4`}>
-              <li className="py-3 px-5 whitespace-nowrap ">
-                <a href="https://ecotourismbali.com/blog/">Blog</a>
-              </li>
-              <li className="py-3 px-5 whitespace-nowrap ">
-                <a href="https://ecotourismbali.com/news/">News</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
+          <span>|</span>
+          <a
+            href="https://member.ecotourismbali.com/login"
+            class=" whitespace-nowrap"
+          >
+            Sign In
+          </a>
+        </div>
       </div>
     </header>
   );
