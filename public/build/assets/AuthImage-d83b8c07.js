55eb081e51799556373f82f21f0bb7b34ffe098c
Muhamad Hafiz <m.hafiz1825@gmail.com>
2024-08-18 14:31:41 +0700

    update build donation

--- public/build/assets/AuthImage-d83b8c07.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./app-2d337225.js";const l="/build/assets/authImage-d4c8c53f.jpg";function t({image:e}){return a("div",{className:"pr-12 h-full",children:a("img",{className:"rounded-2xl w-full h-full object-cover min-h-[70vh]",src:e||l,alt:""})})}export{t as A};
---
+++ public/build/assets/TitlePage-018850d0.js
@@ -0,0 +1,1 @@
+import{a as e}from"./app-a32fd381.js";import{u as l}from"./useTranslation-52c64420.js";function n({title:t,className:o}){const{t:a}=l();return e("h1",{className:"font-bold text-2xl lg:text-3xl mb-6 lg:mb-10 "+o,children:a(t)})}export{n as T};
