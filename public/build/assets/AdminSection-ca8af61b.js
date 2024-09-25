17918eac677dcee9ce74c71efbc11e44bc8a07a5
Muhamad Hafiz <m.hafiz1825@gmail.com>
2024-09-25 21:05:41 +0700

    update build

--- public/build/assets/AdminSection-ca8af61b.js
+++
@@ -1,1 +0,0 @@
-import{a as d}from"./app-9bf0fb01.js";function n({children:o,className:a=""}){return d("div",{className:`bg-white drop-shadow-admin p-6 rounded-2xl ${a}`,children:o})}export{n as A};
---
+++ public/build/assets/BackTo-d9114482.js
@@ -0,0 +1,1 @@
+import{a as s,j as t,d as n}from"./app-9ddcf5bf.js";import{F as m,o as c}from"./index-11fa6b5e.js";import{u as i}from"./useTranslation-3d2a51b3.js";function u({link:o,title:a,className:e}){const{t:r}=i();return s("div",{children:t(n,{href:o,className:`mb-6 flex items-center ${e}`,children:[s(m,{icon:c,className:"mr-2"}),r(a)]})})}export{u as default};
