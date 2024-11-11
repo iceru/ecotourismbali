8e0e8f5164d4075ccea2d7d75df225df4daa2863
Muhamad Hafiz <m.hafiz1825@gmail.com>
2024-11-11 14:53:45 +0700

    update payment discount

---
+++ public/build/assets/TitleSection-965aee97.js
@@ -0,0 +1,1 @@
+import{a as t}from"./app-35790694.js";import{u as n}from"./useTranslation-88aab21a.js";function s({title:o,className:e}){const{t:i}=n();return t("div",{children:t("div",{className:`font-bold text-xl ${e}`,children:i(o)})})}export{s as default};
