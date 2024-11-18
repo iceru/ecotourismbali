6519736c989b7991e2e76927700887a01249eea1
Muhamad Hafiz <m.hafiz1825@gmail.com>
2024-11-18 21:17:17 +0700

    update build

---
+++ public/build/assets/TitleSection-ce171fa1.js
@@ -0,0 +1,1 @@
+import{a as t}from"./app-1ec90d50.js";import{u as n}from"./useTranslation-d3b18344.js";function s({title:o,className:e}){const{t:i}=n();return t("div",{children:t("div",{className:`font-bold text-xl ${e}`,children:i(o)})})}export{s as default};
