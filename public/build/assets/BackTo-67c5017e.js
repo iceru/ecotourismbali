fd085a9d3d18f935cf336e89d021c4c9c1c31ecc
Muhamad Hafiz <m.hafiz1825@gmail.com>
2024-11-24 21:44:02 +0700

    update build

--- public/build/assets/BackTo-67c5017e.js
+++
@@ -1,1 +0,0 @@
-import{a as s,j as t,d as n}from"./app-1ec90d50.js";import{F as m,o as c}from"./index-4cfdcf06.js";import{u as i}from"./useTranslation-d3b18344.js";function u({link:o,title:a,className:e}){const{t:r}=i();return s("div",{children:t(n,{href:o,className:`mb-6 flex items-center ${e}`,children:[s(m,{icon:c,className:"mr-2"}),r(a)]})})}export{u as default};
---
+++ public/build/assets/BackTo-a7e35a4b.js
@@ -0,0 +1,1 @@
+import{a as s,j as t,d as n}from"./app-a241506e.js";import{F as m,o as c}from"./index-1347b3ff.js";import{u as i}from"./useTranslation-ac2a06be.js";function u({link:o,title:a,className:e}){const{t:r}=i();return s("div",{children:t(n,{href:o,className:`mb-6 flex items-center ${e}`,children:[s(m,{icon:c,className:"mr-2"}),r(a)]})})}export{u as default};
