9eca794a9416b92af8aa7dc079e11f16688da3f0
Muhamad Hafiz <m.hafiz1825@gmail.com>
2024-11-26 20:27:29 +0700

    update text donation success

--- resources/js/Pages/DonationSuccess.jsx
+++ resources/js/Pages/DonationSuccess.jsx
@@ -2,27 +2,24 @@ import Button from '@/Components/Button';
 import Guest from '@/Layouts/GuestLayout';
 import React from 'react';
 import donationImg from '../../images/donation.png';
+import { useTranslation } from 'react-i18next';
 
 function DonationSucces() {
+  const { t } = useTranslation();
   return (
     <Guest>
       <div className="flex flex-col justify-center items-center">
         <h1 className="text-center text-4xl font-bold mb-4">
-          Thank You for the Donations!
+          {t('success_dono_title')}
         </h1>
-        <p className="text-center lg:w-3/4 mb-4">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
-          architecto dolore itaque illum aspernatur perferendis possimus
-          veritatis aliquam doloribus quibusdam? Deserunt non ullam tenetur
-          voluptas nulla? Quis nesciunt vitae id.
-        </p>
+        <p className="text-center lg:w-3/4 mb-4">{t('success_dono_text')}</p>
         <Button
           as="link"
           color="secondary"
           href="https://ecotourismbali.com"
           className="mb-4"
         >
-          Back to Homepage
+          {t('success_dono_btn')}
         </Button>
 
         <div>
