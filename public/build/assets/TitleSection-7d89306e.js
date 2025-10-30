70a33acf50e82e0c198e60b5b86462013afe1ebd
Muhamad Hafiz <muhamad.hafiz@margocity.com>
2025-10-30 14:04:40 +0700

    update build

--- public/build/assets/TitleSection-7d89306e.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./app-1d322ffc.js";import{u as n}from"./useTranslation-c69d9c88.js";function s({title:o,className:e}){const{t:i}=n();return t("div",{children:t("div",{className:`font-bold text-xl ${e}`,children:i(o)})})}export{s as default};
---
+++ public/build/assets/TitleSection-92e43f27.js
@@ -0,0 +1,1 @@
+import{a as t}from"./app-ccad20eb.js";import{u as n}from"./useTranslation-b50d78c4.js";function s({title:o,className:e}){const{t:i}=n();return t("div",{children:t("div",{className:`font-bold text-xl ${e}`,children:i(o)})})}export{s as default};
