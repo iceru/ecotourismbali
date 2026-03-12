01dca4c9c4ffb9f4d5ff90d61dcf8f67aeefee6a
Muhamad Hafiz <muhamad.hafiz@margocity.com>
2026-03-12 09:15:52 +0700

    update a in assessments

---
+++ public/build/assets/BackTo-2fa89f52.js
@@ -0,0 +1,1 @@
+import{a as s,j as t,d as n}from"./app-c1949ee5.js";import{F as m,q as c}from"./index-204a84b3.js";import{u as i}from"./useTranslation-d78d4824.js";function u({link:a,title:e,className:o}){const{t:r}=i();return s("div",{children:t(n,{href:a,className:`mb-6 flex items-center ${o}`,children:[s(m,{icon:c,className:"mr-2"}),r(e)]})})}export{u as default};
