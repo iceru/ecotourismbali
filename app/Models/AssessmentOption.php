bdda652314b2d28163326a56a79533b174aada6e
Muhamad Hafiz <m.hafiz1825@gmail.com>
2026-04-11 19:13:43 +0700

    add softdeletes on assessment options

--- app/Models/AssessmentOption.php
+++ app/Models/AssessmentOption.php
@@ -4,12 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; // 1. Import the trait
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class AssessmentOption extends Model
 {
+    use HasFactory, SoftDeletes; // 2. Use the trait here
+
     protected $table = 'assessment_options';
+
+    // To ensure the deleted_at column is treated as a Carbon instance
+    protected $dates = ['deleted_at'];
 
     public function assessment_question(): BelongsTo
     {
