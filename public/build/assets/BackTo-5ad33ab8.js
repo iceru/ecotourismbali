4804543665e2e2bd5d279d42d9f608aabd6224fe
Muhamad Hafiz <m.hafiz1825@gmail.com>
2026-05-19 19:24:03 +0700

    update build

---
+++ public/build/assets/BackTo-5ad33ab8.js
@@ -0,0 +1,1 @@
+import{a as s,j as t,d as n}from"./app-db356a42.js";import{F as m,q as c}from"./index-0f96267c.js";import{u as i}from"./useTranslation-312678a8.js";function u({link:a,title:e,className:o}){const{t:r}=i();return s("div",{children:t(n,{href:a,className:`mb-6 flex items-center ${o}`,children:[s(m,{icon:c,className:"mr-2"}),r(e)]})})}export{u as default};
